62b9044a0c5f0bf9eb6d94089fb3c61f64722ac5
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-01-12 14:53:49 +0000

    New translations web_server.py (Dutch)

--- nl-NL/solutions/web_server.py
+++ nl-NL/solutions/web_server.py
@@ -5,12 +5,12 @@ from picozero import pico_temp_sensor, pico_led
 import machine
 
 
-ssid = 'SSID here'
+ssid = 'SSID hier''
 password = 'psk_here'
 
 
 def connect():
-    #Connect to WLAN
+    #Maak verbinding met WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
