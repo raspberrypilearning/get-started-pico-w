39b4f4c4cd78af58c4cb3bdeebaa755ba4489f29
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-01-12 16:18:36 +0000

    New translations web_server.py (Dutch)

--- nl-NL/solutions/web_server.py
+++ nl-NL/solutions/web_server.py
@@ -15,10 +15,10 @@ def connect():
     wlan.active(True)
     wlan.connect(ssid, password)
     while wlan.isconnected() == False:
-        print('Waiting for connection...')
+        print('Wachten op verbinding...')
         sleep(1)
     ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
+    print(f'Verbonden op {ip}')
     return ip
     
 
